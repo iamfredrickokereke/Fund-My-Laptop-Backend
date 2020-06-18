c8b9cef8e3d6b485c1116021829326e0d16259f5
Chiagozie Robert-Maduekwe <chiagozier@yahoo.com>
2020-06-17 23:25:27 -0400

    Implemented twitter authentication

--- server.js
+++ server.js
@@ -1,19 +1,45 @@
+require('dotenv').config();
 // require('express-async-errors')
 const http = require('http');
 const express = require('express');
 const path = require("path")
+const passport = require('passport'); 
+const { Strategy } = require('passport-twitter'); 
+const { TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, SESSION_SECRET } =  process.env;
 const app = express();
 const server = http.createServer(app);
 
-// const preMiddlewares = require('./src/middlewares/preMiddlewares');
+ //const preMiddlewares = require('./src/middlewares/preMiddlewares');
 // const errorMiddlewares = require('./src/middlewares/errorMiddlewares');
-// const routes = require('./src/routes');
+const routes = require('./src/routes');
 // const databaseConfig = require('./src/config');
 const port = 2020 || process.env.PORT;
 
 // preMiddlewares(app);
+passport.use(new Strategy({  
+   consumerKey: TWITTER_CONSUMER_KEY, 
+   consumerSecret: TWITTER_CONSUMER_SECRET, 
+   callbackURL: 'https://www.fundmylaptop.com/' 
+},   
+(accessToken, refreshToken, profile, cb) => {     
+	return cb(null, profile); }));  
+	
+passport.serializeUser((user, cb) => {   
+	cb(null, user); 
+});  
+	
+passport.deserializeUser((obj, cb) => {   
+	cb(null, obj); 
+}); 
 
-// app.use('/api', routes())
+app.use(require('express-session')({ 
+	secret: SESSION_SECRET, resave: true, saveUninitialized: true })); 
+
+app.use(passport.initialize()); 
+
+app.use(passport.session());
+
+app.use('/api', routes)
 
 app.use('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname+'/public/index.html'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,17 @@
+const passport = require('passport');
 const router = require("express").Router();
 
 
 router.get("/test", (req, res) => res.send("Yeah it works!"));
-
+router.get('/login/twitter', passport.authenticate('twitter'));  
+router.get('/logout', (req, res, next) => {   
+	req.logout();   
+	res.redirect('/'); 
+});  
+router.get('/return',    passport.authenticate('twitter', { failureRedirect: '/' }),   
+	(req, res, next) => {     res.redirect('/'); 
+	
+}); 
 module.exports = router
 
 
